4438d892617b3a53431030fad16625dadc427efe
physics3D <kroener.tobias@t-online.de>
2020-10-18 16:09:35 +0200

    added arrary function

--- js/functions.js
+++ js/functions.js
@@ -1,180 +1,209 @@
 function init() {
-	for (let i = 0; i < POPSIZE; i++) {
-		agents.push(new Dino());
-	}
+  for (let i = 0; i < POPSIZE; i++) {
+    agents.push(new Dino());
+  }
 }
 
 function generateObstacles() {
-	//ein obstacle für den start
-	obstacles.push(new Obstacle(int(random(OBSTACLE_MIN_DISTANCE, OBSTACLE_MAX_DISTANCE))));
+  //ein obstacle für den start
+  obstacles.push(new Obstacle(int(random(OBSTACLE_MIN_DISTANCE, OBSTACLE_MAX_DISTANCE))));
 
-	for (let i = 0; i < 3; i++) {
-		generateNewObstacle();
-	}
+  for (let i = 0; i < 3; i++) {
+    generateNewObstacle();
+  }
 }
 
 function runAI() {
 
-	if (agentsAlive < 1) {
-		let av = neat.getAverage();
-		neat.sort();
-		drawGraph(neat.population[0].graph($('.best').width() / 2, $('.best').height() / 2), '.best');
-		console.log('Durchschnitt letzter Generation: ' + av);
-		averageScores.push(av);
-		charts();
-		neat.evolve();
-		obstacles = [];
-		scoresThisRound = [];
-		generateObstacles();
-		for (let i in agents) {
-			agents[i].reset();
-		}
-		distanceWalked = 0;
-		agentsAlive = POPSIZE;
-	}
+  if (agentsAlive < 1) {
+    let av = neat.getAverage();
+    neat.sort();
+    drawGraph(neat.population[0].graph($('.best').width() / 2, $('.best').height() / 2), '.best');
+    console.log('Durchschnitt letzter Generation: ' + av);
+    averageScores.push(av);
+    charts();
+    neat.evolve();
+    obstacles = [];
+    scoresThisRound = [];
+    generateObstacles();
+    for (let i in agents) {
+      agents[i].reset();
+    }
+    distanceWalked = 0;
+    agentsAlive = POPSIZE;
+  }
 
-	// agents und obstacles colliden
-	for (let i in agents) {
-		for (let j in obstacles) {
-			if (agents[i].alive && obstacles[j].collide(agents[i])) {
-				agents[i].alive = false;
-				neat.population[i].score = realDistance();
-				scoresThisRound.push(realDistance());
-				agentsAlive--;
-			}
-		}
-	}
+  // agents und obstacles colliden
+  for (let i in agents) {
+    for (let j in obstacles) {
+      if (agents[i].alive && obstacles[j].collide(agents[i])) {
+        agents[i].alive = false;
+        neat.population[i].score = realDistance();
+        scoresThisRound.push(realDistance());
+        agentsAlive--;
+      }
+    }
+  }
 
-	let distDtoO = 1 - softSign(distanceFromDinoToNextObstacle());
-	let distDtoD = 1 - softSign(distanceFromDinoToNextDragon());
-	// console.log(distDtoO, distDtoD);
-	// NN ausführen
-	for (let i in agents) {
-		if (agents[i].alive) {
-			let output = neat.population[i].activate([distDtoO, distDtoD, agents[i].jumping]);
-			if (output[0] > 0.5) {
-				agents[i].jump();
-			}
-			if (output[1] > 0.5) {
-				agents[i].crouch();
-			} else {
-				agents[i].uncrouch();
-			}
+  let distDtoO = 1 - softSign(distanceFromDinoToNextObstacle());
+  let distDtoD = 1 - softSign(distanceFromDinoToNextDragon());
+  // console.log(distDtoO, distDtoD);
+  // NN ausführen
+  for (let i in agents) {
+    if (agents[i].alive) {
+      let output = neat.population[i].activate([distDtoO, distDtoD, agents[i].jumping]);
+      if (output[0] > 0.5) {
+        agents[i].jump();
+      }
+      if (output[1] > 0.5) {
+        agents[i].crouch();
+      } else {
+        agents[i].uncrouch();
+      }
 
-			agents[i].update();
-			agents[i].draw();
-		}
-	}
+      agents[i].update();
+      agents[i].draw();
+    }
+  }
 
-	// obstacles updaten & drawen
-	for (let i in obstacles) {
-		obstacles[i].update();
-		obstacles[i].draw();
-	}
+  // obstacles updaten & drawen
+  for (let i in obstacles) {
+    obstacles[i].update();
+    obstacles[i].draw();
+  }
 
 }
 
 function runPLAYER() {
-	for (let i in obstacles) {
-		//collisions
-		if (obstacles[i].collide(dino)) { // wenn collidiert neustart
-			distanceWalked = 0;
-			obstacles = [];
-			setup(); // Notiz: die setup funktion sollte nur einmal ausgeführt werden
-		}
-		obstacles[i].update();
-		obstacles[i].draw();
-	}
+  for (let i in obstacles) {
+    //collisions
+    if (obstacles[i].collide(dino)) { // wenn collidiert neustart
+      distanceWalked = 0;
+      obstacles = [];
+      setup(); // Notiz: die setup funktion sollte nur einmal ausgeführt werden
+    }
+    obstacles[i].update();
+    obstacles[i].draw();
+  }
 
-	dino.update();
-	dino.draw();
+  dino.update();
+  dino.draw();
 }
 
 function generateNewObstacle() {
 
-	// finde obstacle das am weitesten rechts ist
-	let maxx = 0;
+  // finde obstacle das am weitesten rechts ist
+  let maxx = 0;
 
-	for (let i = 0; i < obstacles.length; i++) {
-		if (obstacles[i].x > maxx) {
-			maxx = obstacles[i].x;
-		}
-	}
+  for (let i = 0; i < obstacles.length; i++) {
+    if (obstacles[i].x > maxx) {
+      maxx = obstacles[i].x;
+    }
+  }
 
-	// wenn player schon weit genug gelaufen & Zufall
-	if (realDistance() > MIN_WALKED_DISTANCE_FOR_DRAGONS && random() < DRAGON_PROBABILITY) {
+  // wenn player schon weit genug gelaufen & Zufall
+  if (realDistance() > MIN_WALKED_DISTANCE_FOR_DRAGONS && random() < DRAGON_PROBABILITY) {
 
-		//fliegendes Obstacle
-		obstacles.push(new Dragon(maxx + int(random(OBSTACLE_MIN_DISTANCE, OBSTACLE_MAX_DISTANCE)), CANVAS_HEIGHT - DRAGON_HEIGHT - DRAGON_HEIGHT_WHERE_THEY_FLY, DRAGON_WIDTH, DRAGON_HEIGHT, DRAGON_SPEED));
-	} else {
+    //fliegendes Obstacle
+    obstacles.push(new Dragon(maxx + int(random(OBSTACLE_MIN_DISTANCE, OBSTACLE_MAX_DISTANCE)), CANVAS_HEIGHT - DRAGON_HEIGHT - DRAGON_HEIGHT_WHERE_THEY_FLY, DRAGON_WIDTH, DRAGON_HEIGHT, DRAGON_SPEED));
+  } else {
 
-		//normales Obstacle
-		obstacles.push(new Obstacle(maxx + int(random(OBSTACLE_MIN_DISTANCE, OBSTACLE_MAX_DISTANCE)), CANVAS_HEIGHT - OBSTACLE_HEIGHT, OBSTACLE_WIDTH, OBSTACLE_HEIGHT));
-	}
+    //normales Obstacle
+    obstacles.push(new Obstacle(maxx + int(random(OBSTACLE_MIN_DISTANCE, OBSTACLE_MAX_DISTANCE)), CANVAS_HEIGHT - OBSTACLE_HEIGHT, OBSTACLE_WIDTH, OBSTACLE_HEIGHT));
+  }
 }
 
 function distanceFromDinoToNextObstacle() { // distanz von Dino zu obstacle direkt rechts neben ihm
 
-	// finde obstacle dessen x am kleinsten aber rechts von dino
-	let minx = Infinity;
+  // finde obstacle dessen x am kleinsten aber rechts von dino
+  let minx = Infinity;
 
-	for (let i in obstacles) {
-		if (obstacles[i] instanceof Obstacle && obstacles[i].x < minx && obstacles[i].x > DINO_X + DINO_WIDTH) {
-			minx = obstacles[i].x;
-		}
-	}
+  for (let i in obstacles) {
+    if (obstacles[i] instanceof Obstacle && obstacles[i].x < minx && obstacles[i].x > DINO_X + DINO_WIDTH) {
+      minx = obstacles[i].x;
+    }
+  }
 
-	//pos vom obstacle minus die vom dino und die breite des dinos, weil wir die distanz von rechte kante dino zu linker kante obstacle wollen
-	return minx !== Infinity ? minx - DINO_X - DINO_WIDTH : CANVAS_WIDTH - DINO_X;
+  //pos vom obstacle minus die vom dino und die breite des dinos, weil wir die distanz von rechte kante dino zu linker kante obstacle wollen
+  return minx !== Infinity ? minx - DINO_X - DINO_WIDTH : CANVAS_WIDTH - DINO_X;
 }
 
 function distanceFromDinoToNextDragon() { // distanz von Dino zu dragon direkt rechts neben ihm
 
-	// finde dragon dessen x am kleinsten aber rechts von dino
-	let minx = Infinity;
+  // finde dragon dessen x am kleinsten aber rechts von dino
+  let minx = Infinity;
 
-	for (let i in obstacles) {
-		if (obstacles[i] instanceof Dragon && obstacles[i].x < minx && obstacles[i].x > DINO_X + DINO_WIDTH) {
-			minx = obstacles[i].x;
-		}
-	}
+  for (let i in obstacles) {
+    if (obstacles[i] instanceof Dragon && obstacles[i].x < minx && obstacles[i].x > DINO_X + DINO_WIDTH) {
+      minx = obstacles[i].x;
+    }
+  }
 
-	//pos vom dragon minus die vom dino und die breite des dinos, weil wir die distanz von rechte kante dino zu linker kante dragon wollen
-	return minx !== Infinity ? minx - DINO_X - DINO_WIDTH : CANVAS_WIDTH - DINO_X;
+  //pos vom dragon minus die vom dino und die breite des dinos, weil wir die distanz von rechte kante dino zu linker kante dragon wollen
+  return minx !== Infinity ? minx - DINO_X - DINO_WIDTH : CANVAS_WIDTH - DINO_X;
+}
+
+function NextObstacleTypeDoenstMatter() { // distanz von Dino zu dragon direkt rechts neben ihm
+
+  // finde dragon dessen x am kleinsten aber rechts von dino
+  let minx = Infinity;
+  let obstacle;
+
+  for (let i in obstacles) {
+    if (obstacles[i].x < minx && obstacles[i].x > DINO_X + DINO_WIDTH) {
+      minx = obstacles[i].x;
+      obstacle = obstacles[i];
+    }
+  }
+
+  //pos vom dragon minus die vom dino und die breite des dinos, weil wir die distanz von rechte kante dino zu linker kante dragon wollen
+  return obstacle;
 }
 
 function realDistance() {
-	return int(distanceWalked / DISTANCE_COEFFICIENT);
+  return int(distanceWalked / DISTANCE_COEFFICIENT);
 }
 
 function softSign(x) {
-	return x / (20 + Math.abs(x));
+  return x / (20 + Math.abs(x));
 }
 
 function charts() {
-	// Balkendiagramm
-	var data = [{
-		x: averageScores.length,
-		y: averageScores,
-		type: 'bar'
-	}
-	];
-	var layout = {
-		title: "Durschnittliche Punktzahl",
-		xaxis: { title: "Generation" },
-		yaxis: { title: "Punktzahl" }
-	}
-	Plotly.newPlot('chart1', data, layout);
+  // Balkendiagramm
+  var data = [{
+    x: averageScores.length,
+    y: averageScores,
+    type: 'bar'
+  }
+  ];
+  var layout = {
+    title: "Durschnittliche Punktzahl",
+    xaxis: { title: "Generation" },
+    yaxis: { title: "Punktzahl" }
+  }
+  Plotly.newPlot('chart1', data, layout);
 
-	// Histogramm
-	var data = {
-		x: scoresThisRound,
-		type: 'histogram',
-	};
-	var layout = {
-		title: "Punktzahl der Agenten der letzten Generation",
-		xaxis: { title: "Erreichte Punktzahl" },
-		yaxis: { title: "Anzahl der Agenten" }
-	}
-	Plotly.newPlot('chart2', [data], layout);
+  // Histogramm
+  var data = {
+    x: scoresThisRound,
+    type: 'histogram',
+  };
+  var layout = {
+    title: "Punktzahl der Agenten der letzten Generation",
+    xaxis: { title: "Erreichte Punktzahl" },
+    yaxis: { title: "Anzahl der Agenten" }
+  }
+  Plotly.newPlot('chart2', [data], layout);
+}
+
+// das ist die Funktion die den Array ausgibt, gib ihr mal nen besseren Namen :)
+
+function returnArray() {
+  let nextObstacle = distanceFromDinoToNextObstacle();
+
+  let dist = nextObstacle.x - DINO_X - nextObstacle.width;
+  let type = (nextObstacle instanceof Obstacle) ? 0 : 1;
+  let height = nextObstacle.y + nextObstacle.height; // das ist jetzt die unterkante wenn du die oberkante willst musst du + nextObstacle.height wegmachen
+
+  return [dist, type, height];
 }
